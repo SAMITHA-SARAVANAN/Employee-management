32e7516a39636d97be77445f234a8c837321f09c
Samitha G S <121777231+SAMITHA-SARAVANAN@users.noreply.github.com>
2025-03-02 20:08:42 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,22 +1,26 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for empproj project.
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.employee_login, name='employee_login'),
-    path('admin-login', views.login_view, name='login'),    
-    path('index/', views.index, name='index'),
-    path('user-dashboard/', views.employee_details, name='user_dashboard'),  
-    path('submit-request/', views.submit_request, name='submit_request'),
-    path('admin-dashboard/', views.dashboard_with_notifications, name='admin_dashboard'),
-    path('clear-notifications/', views.clear_notifications, name='clear_notifications'),
-    
-    #path('', views.index, name="index"),
-    path('view_info/<int:id>/', views.view_info, name='view_info'),
-    path('add_employee/', views.add_employee, name='add_employee'),
-    path('delete_employee/<int:id>/', views.delete_employee, name='delete_employee'),
-    path('update/<int:id>/', views.update_employee, name='update_employee'), 
-    path('logout/', views.logout_view, name='logout'),   
+    path('admin/', admin.site.urls),
+    path('', include('empapp.urls')),
 
 ]
+
 
